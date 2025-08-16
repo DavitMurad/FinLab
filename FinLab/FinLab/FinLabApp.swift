3f34ba97d14aa4dc5b5b7cc2a9e807c79c962d84
“Davit <2840850M@student.gla.ac.uk>
2025-08-16 22:13:56 +0100

    Initial commit

---
+++ FinLab/FinLab/FinLabApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinLabApp.swift
+//  FinLab
+//
+//  Created by Davit Muradyan on 16.08.25.
+//
+
+import SwiftUI
+
+@main
+struct FinLabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
